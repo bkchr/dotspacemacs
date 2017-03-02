7a9dd49aca6721b3dcdf27335b3e58956ad6b1a6
Bastian Köcher <git@kchr.de>
2017-03-02 08:11:50 +0100

    Adds missing changes

--- common.el
+++ common.el
@@ -3,5 +3,21 @@
 (with-eval-after-load 'linum
     (linum-relative-toggle))
 
-;; Do not paste the highlighted text when it should be replaced 
+;; Do not paste the highlighted text when it should be replaced
 (setq save-interprogram-paste-before-kill t)
+
+(defun my-save-if-bufferfilename ()
+  (if (buffer-file-name)
+      (progn
+        (save-buffer)
+        )
+    )
+)
+
+;; save on leaving evil mode
+(add-hook 'evil-insert-state-exit-hook 'my-save-if-bufferfilename)
+
+;; don't yell at me about tags
+(setq large-file-warning-threshold nil)
+(setq dotspacemacs-large-file-size nil)
+
