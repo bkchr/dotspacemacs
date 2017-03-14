5bd9327e88a9dcdbc1b60acbe867fc668f8e5353
Bastian Köcher <git@kchr.de>
2017-03-14 23:31:11 +0100

    Adds rust snippets

--- rust.el
+++ rust.el
@@ -1,4 +1,3 @@
-
 ;;(add-hook 'rust-mode-hook 'ycmd-mode)
 (defun my-rust-column-length ()
   (setq-default fci-rule-column 80)
@@ -8,3 +7,8 @@
 (add-hook 'rust-mode-hook 'my-rust-column-length)
 
 (spacemacs/add-to-hooks 'turn-on-fci-mode '(rust-mode-hook))
+
+(add-to-list 'load-path "~/.spacemacs.d/plugins/rust-snippets")
+(autoload 'rust-snippets/initialize "rust-snippets")
+(eval-after-load 'yasnippet
+  '(rust-snippets/initialize))
