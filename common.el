a3f07a0882ac457f86186f956ec8c636a75199db
Bastian Köcher <git@kchr.de>
2017-03-16 13:41:20 +0100

    Adds neotree functionality to automatically refresh on buffer change

--- common.el
+++ common.el
@@ -1,23 +1,20 @@
 (global-linum-mode)
 
 (with-eval-after-load 'linum
-    (linum-relative-toggle))
+  (linum-relative-toggle))
 
 ;; Do not paste the highlighted text when it should be replaced
-(setq save-interprogram-paste-before-kill t)
+(setq save-interprogram-paste-before-kill
+      t)
 
 (defun my-save-if-bufferfilename ()
   (if (buffer-file-name)
       (progn
-        (save-buffer)
-        )
-    )
-)
+        (save-buffer))))
 
 ;; save on leaving evil mode
 (add-hook 'evil-insert-state-exit-hook 'my-save-if-bufferfilename)
 
 ;; don't yell at me about tags
 (setq large-file-warning-threshold nil)
-(setq dotspacemacs-large-file-size nil)
-
+(setq dotspacemacs-large-file-size 9999999)
