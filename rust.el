74e197dfd7954096f8c33996fd4dd7f1c07959ed
Bastian Köcher <git@kchr.de>
2017-05-10 22:18:48 +0200

    Some stuff

--- rust.el
+++ rust.el
@@ -1,12 +1,12 @@
 ;;(add-hook 'rust-mode-hook 'ycmd-mode)
 (defun my-rust-column-length ()
-  (setq-default fci-rule-column 100)
-)
+  (setq-default fci-rule-column 100))
 
 ;; save on leaving evil mode
 (add-hook 'rust-mode-hook 'my-rust-column-length)
 
-(spacemacs/add-to-hooks 'turn-on-fci-mode '(rust-mode-hook))
+(spacemacs/add-to-hooks 'turn-on-fci-mode
+                        '(rust-mode-hook))
 
 (add-to-list 'load-path "~/.spacemacs.d/plugins/rust-snippets")
 (autoload 'rust-snippets/initialize "rust-snippets")
